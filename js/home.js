dbb9d8381b7a0dea64e512cfa712a29f65add046
votuanhuy2910 <votuanhuy2910@gmail.com>
2023-02-18 16:37:00 +0700

    update thumb

--- js/home.js
+++ js/home.js
@@ -1,8 +1,28 @@
-let playLists = document.querySelectorAll(".playlist h4");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const playLists = $$(".playlist h4");
 
 playLists.forEach((item) => {
 	item.onclick = function () {
 		document.querySelector(".playlist .active").classList.remove("active");
 		this.classList.add("active");
+	};
+});
+
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+
+const tabActive = $(".tab-item.active");
+
+tabs.forEach((tab, index) => {
+	const pane = panes[index];
+
+	tab.onclick = function () {
+		$(".tab-item.active").classList.remove("active");
+		$(".tab-pane.active").classList.remove("active");
+
+		this.classList.add("active");
+		pane.classList.add("active");
 	};
 });
