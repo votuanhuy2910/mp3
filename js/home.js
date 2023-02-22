9b137b68234e2747e464ec11f7969d2fd6d70f21
votuanhuy2910 <votuanhuy2910@gmail.com>
2023-02-22 23:08:55 +0700

    update

--- js/home.js
+++ js/home.js
@@ -1,34 +1,34 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const toggleBtn = document.getElementById("toggle-btn");
-const body = document.body;
-const darkMode = localStorage.getItem("dark-mode");
+// const toggleBtn = document.getElementById("toggle-btn");
+// const body = document.body;
+// const darkMode = localStorage.getItem("dark-mode");
 
-const enableDarkMode = () => {
-	toggleBtn.classList.replace("ti-sun", "ti-moon");
-	body.classList.add("dark");
-	localStorage.setItem("dark-mode", "enable");
-};
+// const enableDarkMode = () => {
+// 	toggleBtn.classList.replace("ti-sun", "ti-moon");
+// 	body.classList.add("dark");
+// 	localStorage.setItem("dark-mode", "enable");
+// };
 
-const disableDarkMode = () => {
-	toggleBtn.classList.replace("ti-moon", "ti-sun");
-	body.classList.remove("dark");
-	localStorage.setItem("dark-mode", "disable");
-};
+// const disableDarkMode = () => {
+// 	toggleBtn.classList.replace("ti-moon", "ti-sun");
+// 	body.classList.remove("dark");
+// 	localStorage.setItem("dark-mode", "disable");
+// };
 
-if (darkMode === "enable") {
-	enableDarkMode();
-}
+// if (darkMode === "enable") {
+// 	enableDarkMode();
+// }
 
-toggleBtn.onclick = (e) => {
-	darkMode = localStorage.getItem("dark-mode");
-	if (darkMode === "disable") {
-		enableDarkMode();
-	} else {
-		disableDarkMode();
-	}
-};
+// toggleBtn.onclick = (e) => {
+// 	darkMode = localStorage.getItem("dark-mode");
+// 	if (darkMode === "disable") {
+// 		enableDarkMode();
+// 	} else {
+// 		disableDarkMode();
+// 	}
+// };
 
 const playLists = $$(".playlist h4");
 
@@ -51,7 +51,7 @@ lines.forEach((item) => {
 const tabs = $$(".tab-item");
 const panes = $$(".tab-pane");
 
-const tabActive = $(".tab-item.active");
+// const tabActive = $(".tab-item.active");
 
 tabs.forEach((tab, index) => {
 	const pane = panes[index];
