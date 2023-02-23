01dd69e0360a09c2517123f29b7b6b9feb58b0b6
votuanhuy2910 <votuanhuy2910@gmail.com>
2023-02-23 23:12:02 +0700

    update logic

--- js/home.js
+++ js/home.js
@@ -1,57 +1,46 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// const toggleBtn = document.getElementById("toggle-btn");
-// const body = document.body;
-// const darkMode = localStorage.getItem("dark-mode");
+const toggleBtn = $(".fa-sun");
+const body = document.body;
+let darkMode = localStorage.getItem("dark-mode");
 
-// const enableDarkMode = () => {
-// 	toggleBtn.classList.replace("ti-sun", "ti-moon");
-// 	body.classList.add("dark");
-// 	localStorage.setItem("dark-mode", "enable");
-// };
+const enableDarkMode = () => {
+	toggleBtn.classList.replace("fa-sun", "fa-moon");
+	body.classList.add("dark");
+	localStorage.setItem("dark-mode", "enabled");
+};
 
-// const disableDarkMode = () => {
-// 	toggleBtn.classList.replace("ti-moon", "ti-sun");
-// 	body.classList.remove("dark");
-// 	localStorage.setItem("dark-mode", "disable");
-// };
+const disableDarkMode = () => {
+	toggleBtn.classList.replace("fa-moon", "fa-sun");
+	body.classList.remove("dark");
+	localStorage.setItem("dark-mode", "disabled");
+};
 
-// if (darkMode === "enable") {
-// 	enableDarkMode();
-// }
+if (darkMode === "enabled") {
+	enableDarkMode();
+}
 
-// toggleBtn.onclick = (e) => {
-// 	darkMode = localStorage.getItem("dark-mode");
-// 	if (darkMode === "disable") {
-// 		enableDarkMode();
-// 	} else {
-// 		disableDarkMode();
-// 	}
-// };
+toggleBtn.onclick = () => {
+	darkMode = localStorage.getItem("dark-mode");
+	if (darkMode === "disabled") {
+		enableDarkMode();
+	} else {
+		disableDarkMode();
+	}
+};
 
-const playLists = $$(".playlist h4");
+// const playLists = $$(".playlist h4");
 
-playLists.forEach((item) => {
-	item.onclick = function () {
-		$(".playlist .active").classList.remove("active");
-		this.classList.add("active");
-	};
-});
-
-// const lines = $$(".mid_side .heading .tab_pane-nav");
-
-// lines.forEach((item) => {
+// playLists.forEach((item) => {
 // 	item.onclick = function () {
-// 		$(".mid_side .heading .active").classList.remove("active");
+// 		$(".playlist .active").classList.remove("active");
 // 		this.classList.add("active");
 // 	};
 // });
 
 const tabs = $$(".tab-item");
 const panes = $$(".tab-pane");
-
-// const tabActive = $(".tab-item.active");
 
 tabs.forEach((tab, index) => {
 	const pane = panes[index];
@@ -65,17 +54,36 @@ tabs.forEach((tab, index) => {
 	};
 });
 
-const tabsLeft = $$(".tab_item-left")
-const panesMid = $$(".tab_pane-mid")
+const tabsLeft = $$(".tab_item-left");
+const panesMid = $$(".tab_pane-nav");
+const contentsMid = $$(".content");
 
 tabsLeft.forEach((tabLeft, index) => {
 	const paneMid = panesMid[index];
+	const contentMid = contentsMid[index];
 
 	tabLeft.onclick = function () {
 		$(".tab_item-left.active").classList.remove("active");
-		$(".tab_pane-mid.active").classList.remove("active");
+		$(".tab_pane-nav.active").classList.remove("active");
+		$(".content.active").classList.remove("active");
 
 		this.classList.add("active");
 		paneMid.classList.add("active");
-	}
-})
+		contentMid.classList.add("active");
+	};
+
+	panesMid.forEach((pane, index) => {
+		const tabLeft = tabsLeft[index];
+		const contentMid = contentsMid[index];
+
+		pane.onclick = function () {
+			$(".tab_item-left.active").classList.remove("active");
+			$(".tab_pane-nav.active").classList.remove("active");
+			$(".content.active").classList.remove("active");
+
+			this.classList.add("active");
+			tabLeft.classList.add("active");
+			contentMid.classList.add("active");
+		};
+	});
+});
